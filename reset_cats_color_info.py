6c2873e68031c38b501f664a1b224a1ef23fa5b1
Igor Kavzov <igor.kavzov@gmail.com>
2019-02-24 18:14:09 +0300

    First commit

---
+++ reset_cats_color_info.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+
+def reset_cat_color_info():
+    host = 'localhost'
+    port = 5432
+    db_name = 'wg_forge_db'
+    db_user = 'wg_forge'
+    db_user_passw = '42a'
+
+    conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_user_passw, host=host, port=port)
+    query = 'DELETE FROM cat_colors_info'
+    with conn.cursor() as cur:
+        try:
+            cur.execute(query)
+        except psycopg2.Error as e:
+            print(e)
+        conn.commit()
+    conn.close()
+
+
+if __name__ == '__main__':
+    reset_cat_color_info()
