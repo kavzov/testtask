cf07e549f7917efe194cfee971c892169d57a519
Igor Kavzov <igor.kavzov@gmail.com>
2019-02-25 13:55:05 +0300

    Refactoring

--- cats_stat.py
+++ cats_stat.py
@@ -27,54 +27,66 @@ def get_length_median(lengths):
 
 
 def get_length_mode(lengths):
-    """  Return list of the most common values of lst """
-    # list of tuples (value, count) SORTED by count
+    """ Return modes lengths list """
+    # list of tuples (value, count) SORTED BY COUNT
     counts = Counter(lengths).most_common()
 
-    # first element - max count
-    # its value to mode list
-    stat_mode = [counts[0][0]]
+    # first element of the counts list - max count
+    # its value to modes list
+    modes = [counts[0][0]]
 
     # max count of the value
     max_count = counts[0][1]
 
-    # check for other values is stat mode too (its count equals max_count), append them to stat_mode
+    # check for other values is modes too (its count equals max_count)
+    # append them to stat_mode
     if len(counts) > 1:
         for i in range(1, len(counts)):
             if counts[i][1] == max_count:
-                stat_mode.append(counts[i][0])
+                modes.append(counts[i][0])
             else:
                 break
-    return stat_mode
+    return modes
 
 
 def stat_to_db(*values):
     """ Insert stat to db """
     with psycopg2.connect(dbname=db_name, user=db_user, password=db_user_passw, host=host, port=port) as conn:
         with conn.cursor() as cur:
-            query = "INSERT INTO cats_stat VALUES (%s, %s, %s, %s, %s, %s)"
-            cur.execute(query, values)
-            conn.commit()
+            try:
+                query = "INSERT INTO cats_stat VALUES (%s, %s, %s, %s, %s, %s)"
+                cur.execute(query, values)
+            except psycopg2.Error as e:
+                print("Psycopg2 error: ", e)
+            else:
+                conn.commit()
 
 
 # ----------- Main ----------- #
 
 
 def run():
+    # tails and whiskers lengths
     tails_and_whiskers = get_tail_and_whiskers_lengths()
 
+    # tails lengths
     tails = [tail['tail_length'] for tail in tails_and_whiskers]
+    # whiskers lengths
     whiskers = [wh['whiskers_length'] for wh in tails_and_whiskers]
 
+    # Means
     tail_length_mean = get_length_mean(tails)
     whiskers_length_mean = get_length_mean(whiskers)
 
+    # Medians
     tail_length_median = statistics.median(tails)
     whiskers_length_median = statistics.median(whiskers)
 
+    # Modes
     tail_length_mode = get_length_mode(tails)
     whiskers_length_mode = get_length_mode(whiskers)
 
+    # Store the values to db
     stat_to_db(tail_length_mean, tail_length_median, tail_length_mode,
                whiskers_length_mean, whiskers_length_median, whiskers_length_mode)
 
