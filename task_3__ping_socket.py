f7e2a1ebdf9bb4969592bcfbcb1b0e3fac9f1c29
Igor Kavzov <igor.kavzov@gmail.com>
2019-02-25 18:56:16 +0300

    Refactoring

--- task_3__ping_socket.py
+++ task_3__ping_socket.py
@@ -6,7 +6,6 @@ def run():
 
     # Socket settings
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('', 8080))
     server_socket.listen(1)
 
@@ -19,7 +18,7 @@ def run():
         # Get request path from request headers
         path = conn.recv(1024).decode().split(' ')[1]
 
-        # if path is '/ping', set the appropriate response message
+        # if path is not '/ping' set empty response message
         if path != proper_path:
             response_msg = '\n'
 
