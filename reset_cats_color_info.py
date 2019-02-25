b4a72d2be35556fbae01452719f063b53cd29dc8
Igor Kavzov <igor.kavzov@gmail.com>
2019-02-25 14:14:19 +0300

    Refactoring

--- reset_cats_color_info.py
+++ reset_cats_color_info.py
@@ -4,15 +4,15 @@ from db_settings import host, port, db_name, db_user, db_user_passw
 
 def reset_cat_color_info():
     """ Delete all info from 'cat_color_info' table """
-    conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_user_passw, host=host, port=port)
-    query = 'DELETE FROM cat_colors_info'
-    with conn.cursor() as cur:
-        try:
-            cur.execute(query)
-        except psycopg2.Error as e:
-            print(e)
-        conn.commit()
-    conn.close()
+    with psycopg2.connect(dbname=db_name, user=db_user, password=db_user_passw, host=host, port=port) as conn:
+        with conn.cursor() as cur:
+            query = 'DELETE FROM cat_colors_info'
+            try:
+                cur.execute(query)
+            except psycopg2.Error as e:
+                print(e)
+            else:
+                conn.commit()
 
 
 if __name__ == '__main__':
