5dbb5b59be7e2697fcf3fa260789dfc5d8faa56d
Igor Kavzov <igor.kavzov@gmail.com>
2019-02-27 15:36:57 +0300

    Add method db_query_realdict

--- db_connect.py
+++ db_connect.py
@@ -1,4 +1,5 @@
 import psycopg2
+import psycopg2.extras
 
 
 def connect():
@@ -9,3 +10,14 @@ def connect():
         user='wg_forge',
         password='42a'
     )
+
+
+def db_query_realdict(query):
+    with connect() as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+            try:
+                cur.execute(query)
+            except psycopg2.Error as e:
+                print("Psycopg2 error: ", e)
+            res = cur.fetchall()
+    return res
