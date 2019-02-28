7dfde074e01f34eb711dfefd0b716cea75dc9ffd
Igor Kavzov <igor.kavzov@gmail.com>
2019-02-28 15:56:58 +0300

    Add tests for order, offset and limit

--- tests/test_query.py
+++ tests/test_query.py
@@ -22,5 +22,31 @@ class TestTask4(unittest.TestCase):
         valid_attrs = {'attribute': ['color', 'name']}
         invalid_attrs = {'attribute': ['id', 'length']}
         self.assertEqual([], self.obj.check_query_errors('/cats', valid_attrs))
-        self.assertIn('invalid attribute', self.obj.check_query_errors('/cats', invalid_attrs)[0])
+        self.assertIn("invalid attribute", self.obj.check_query_errors('/cats', invalid_attrs)[0])
 
+    def test_order(self):
+        valid_order = {'order': ['desc']}
+        invalid_order = {'order': ['misc']}
+        invalid_order_multi = {'order': ['desc', 'asc']}
+        self.assertEqual([], self.obj.check_query_errors('/cats', valid_order))
+        self.assertIn("invalid order", self.obj.check_query_errors('/cats', invalid_order)[0])
+        self.assertIn("'order' parameters given", self.obj.check_query_errors('/cats', invalid_order_multi)[0])
+
+    def test_offset(self):
+        # max_offset = Checker.get_dbtable_size('cats') - 1
+        valid_offset = {'offset': ['10']}
+        invalid_offset_sym = {'offset': ['ten']}
+        invalid_offset_greater = {'offset': ['500']}
+        invalid_offset_multi = {'offset': ['10', '20']}
+        self.assertEqual([], self.obj.check_query_errors('/cats', valid_offset))
+        self.assertIn("invalid offset", self.obj.check_query_errors('/cats', invalid_offset_sym)[0])
+        self.assertIn("greater than allowed", self.obj.check_query_errors('/cats', invalid_offset_greater)[0])
+        self.assertIn("'offset' parameters given", self.obj.check_query_errors('/cats', invalid_offset_multi)[0])
+
+    def test_limit(self):
+        valid_limit = {'limit': ['20']}
+        invalid_limit = {'limit': ['twenty']}
+        invalid_limit_multi = {'limit': ['20', '20']}
+        self.assertEqual([], self.obj.check_query_errors('/cats', valid_limit))
+        self.assertIn("invalid limit", self.obj.check_query_errors('/cats', invalid_limit)[0])
+        self.assertIn("'limit' parameters given", self.obj.check_query_errors('/cats', invalid_limit_multi)[0])
