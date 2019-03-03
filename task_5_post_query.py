6d3e95b514fe57a81aeecd1f45952d364319a1b3
Igor Kavzov <igor.kavzov@gmail.com>
2019-03-03 23:18:42 +0300

    Refactoring

--- task_5_post_query.py
+++ task_5_post_query.py
@@ -5,16 +5,12 @@ from db import db_query, db_table_column_names, dict_to_db
 from cats_colors_info import get_colors
 
 
-DB_TABLE = 'cats'
-
-
 class PostQuery:
     """
     Class for handle POST query.
     Provides methods for checking POST query .
     """
-    @staticmethod
-    def _validate_json(post_data):
+    def _check_json(self, post_data):
         """ Check string for valid JSON. Return error message if it not valid """
         json_error = ''
         post_data_dict = {}
@@ -24,8 +20,7 @@ class PostQuery:
             json_error = 'Error: invalid JSON'
         return json_error, post_data_dict
 
-    @staticmethod
-    def _validate_attrs(post_data, valid_attrs):
+    def _check_attrs(self, post_data, valid_attrs):
         """ Check attributes by comparing with the valid attributes list """
         # sort for correct compare
         valid_attrs = sorted(valid_attrs)
@@ -35,30 +30,20 @@ class PostQuery:
             return 'Error: invalid attributes. Expected exactly these: {}'.\
                 format(', '.join("'{}'".format(attr) for attr in valid_attrs))
 
-    @staticmethod
-    def _check_length(post_data, items):
-        """ Check tail and whiskers length. They must be positive integer """
-        for item in items:
-            length = post_data[item]
-            if length <= 0:
-                return 'Error: {} is not positive integer'.format(item)
-
-    @staticmethod
-    def _name_correct(name):
-        """ Name must contain at least one letter, digits, space, '_' or '-' symbols """
-        return re.match("""^[a-zA-z][a-zA-Z0-9 _-]*$""", name)
-
-    def validate(self, post_data, valid_attrs, valid_colors):
-        """ Validates POST query parameters """
+    def check(self, post_data, valid_attrs, valid_colors):
+        """
+        Check POST query parameters
+        Return dict with POST query as dict and error if occur
+        """
         res = {'dict': {}, 'error': ''}
         # validate json
-        json_error, post_data_dict = self._validate_json(post_data)
+        json_error, post_data_dict = self._check_json(post_data)
         if json_error:
             res['error'] = json_error
             return res
 
         # validate attributes: all required
-        attrs_error = self._validate_attrs(post_data_dict, valid_attrs)
+        attrs_error = self._check_attrs(post_data_dict, valid_attrs)
         if attrs_error:
             res['error'] = attrs_error
             return res
@@ -78,22 +63,19 @@ class PostQuery:
         # validate tail and whiskers type
         for length in ['tail_length', 'whiskers_length']:
             if not isinstance(post_data_dict[length], int):
-                res['error'] = "Error: {} not a number".format(length)
+                res['error'] = "Error: {} is not a number".format(length)
+                return res
+            if post_data_dict[length] <= 0:
+                res['error'] = "Error: {} is not positive integer".format(length)
                 return res
 
         res['dict'] = post_data_dict
         return res
 
-    def check(self, post_data_dict, is_namesake):
-        """
-        Check parameters values of POST query
-        """
-        if is_namesake:
-            return 'Error: cat {} already exists'. format(post_data_dict['name'])
-
-        length_error = self._check_length(post_data_dict, ['tail_length', 'whiskers_length'])
-        if length_error:
-            return length_error
+    def check_namesake(self, namesake):
+        """ Return error message if cat with the same name already exist """
+        if namesake:
+            return 'Error: cat {} already exists'. format(namesake[0])
 
 
 class WGTestHTTPRequestHandler(BaseHTTPRequestHandler):
@@ -102,26 +84,25 @@ class WGTestHTTPRequestHandler(BaseHTTPRequestHandler):
         self.query = PostQuery()
         super().__init__(*args, **kwargs)
 
+    DB_TABLE = 'cats'
+
     def response(self, content):
         """ Send response to client """
         self.wfile.write(json.dumps(content).encode('utf-8'))
 
     # ---- POST request handle ---- #
 
-    @staticmethod
-    def _get_valid_attrs():
+    def _get_valid_attrs(self):
         """ Return allowed attributes list """
-        return db_table_column_names(DB_TABLE)
+        return db_table_column_names(self.DB_TABLE)
 
-    @staticmethod
-    def _get_valid_colors():
+    def _get_valid_colors(self):
         """ Return allowed colors list """
         return get_colors()
 
-    @staticmethod
-    def _exist_namesake(name):
-        """ Return QueryDict with the same name cat """
-        return db_query("SELECT name FROM {} WHERE name='{}'".format(DB_TABLE, name), many=False)
+    def _namesake(self, name):
+        """ Check whether cat with the same name already exist in db """
+        return db_query("SELECT name FROM {} WHERE name='{}'".format(self.DB_TABLE, name), many=False)
 
     def do_POST(self):
         """ Handles POST request """
@@ -135,21 +116,22 @@ class WGTestHTTPRequestHandler(BaseHTTPRequestHandler):
         data_length = int(self.headers['Content-Length'])
         post_data_json = self.rfile.read(data_length).decode('utf-8')
 
-        validation_res = self.query.validate(post_data_json, self._get_valid_attrs(), self._get_valid_colors())
-        if validation_res.get('error'):
-            self.response(validation_res['error'])
+        check_results = self.query.check(post_data_json, self._get_valid_attrs(), self._get_valid_colors())
+        if check_results['error']:
+            self.response(check_results['error'])
             return
 
-        post_data_dict = validation_res['dict']
+        post_data_dict = check_results['dict']
 
-        # if query is valid - check remaining values
-        errors = self.query.check(post_data_dict, self._exist_namesake(post_data_dict['name']))
+        # if query is valid - check for namesake
+        namesake_error = self.query.check_namesake(self._namesake(post_data_dict['name']))
+        if namesake_error:
+            self.response(namesake_error)
+            return
 
-        if errors:
-            self.response(errors)
-        else:
-            dict_to_db(DB_TABLE, post_data_dict) and \
-                self.response("Success: cat {} stored to database".format(post_data_dict['name']))
+        # all right - store cat info to db and send success info to client
+        dict_to_db(self.DB_TABLE, post_data_dict) and \
+            self.response("Success: cat {} stored to database".format(post_data_dict['name']))
 
 
 def run():
