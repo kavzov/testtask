b5b7fe5fe4c946e64f2ea4243cee8a9281ec10f5
Igor Kavzov <igor.kavzov@gmail.com>
2019-03-04 15:37:52 +0300

    Initial settings

---
+++ settings.py
@@ -0,0 +1,6 @@
+# db
+DB_TABLE_NAME = 'cats'
+
+# tests
+VALID_ATTR_NAMES = ['name', 'color', 'tail_length', 'whiskers_length']
+VALID_COLORS = ['black', 'white', 'black & white', 'red', 'red & white', 'red & black & white']
