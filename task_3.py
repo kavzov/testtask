f1ee63dc57a88d0e02fdee8c1e5bb87ed09d8de1
Igor Kavzov <igor.kavzov@gmail.com>
2019-03-05 16:38:24 +0300

    Add sockopt SO_REUSEADDR

--- task_3.py
+++ task_3.py
@@ -7,6 +7,7 @@ def main():
 
     # Socket settings
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('', 8080))
     server_socket.listen(1)
 
