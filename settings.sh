22de2f07c97d247e1a582be62bd0de3a1ebd9113
Igor Kavzov <igor.kavzov@gmail.com>
2019-03-05 14:36:39 +0300

    soft links to tasks files

---
+++ settings.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+touch /usr/local/bin/task1 && ln -s task_1.py /usr/local/bin/task1
+touch /usr/local/bin/task2 && ln -s task_2.py /usr/local/bin/task2
+touch /usr/local/bin/task3 && ln -s task_3.py /usr/local/bin/task3
+touch /usr/local/bin/task4 && ln -s task_4.py /usr/local/bin/task4
+touch /usr/local/bin/task5 && ln -s task_4.py /usr/local/bin/task5
+touch /usr/local/bin/tests && ln -s tests/tests.sh /usr/local/bin/tests
