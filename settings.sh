d8351fbc6b488f873dac241efbe4cc1eb3ddeb2b
Igor Kavzov <igor.kavzov@gmail.com>
2019-03-05 15:36:36 +0300

    Fix link path error

--- settings.sh
+++ settings.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-ln -s task_1.py /usr/local/bin/task1
-ln -s task_2.py /usr/local/bin/task2
-ln -s task_3.py /usr/local/bin/task3
-ln -s task_4.py /usr/local/bin/task4
-ln -s task_4.py /usr/local/bin/task5
-ln -s tests/tests.sh /usr/local/bin/tests
+ln -s /testtask/task_1.py /usr/local/bin/task1
+ln -s /testtask/task_2.py /usr/local/bin/task2
+ln -s /testtask/task_3.py /usr/local/bin/task3
+ln -s /testtask/task_4.py /usr/local/bin/task4
+ln -s /testtask/task_4.py /usr/local/bin/task5
+ln -s /testtask/tests/tests.sh /usr/local/bin/tests
