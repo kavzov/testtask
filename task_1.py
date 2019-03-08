a4e5cd6fad83fcfc3e7cf8cfbdde5b0ba8406a2a
Igor Kavzov <igor.kavzov@gmail.com>
2019-03-08 14:45:30 +0300

    Fix psycopg2 duplicate key error

--- task_1.py
+++ task_1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from collections import Counter
-from utils import db_query, dict_items_to_db
+from utils import db_query, dict_items_to_db, db_table_size, reset_table
 from settings import CATS_TABLE
 
 
@@ -11,6 +11,7 @@ def get_cats_colors():
 
 
 def main():
+    CATS_COLORS_TABLE = 'cat_colors_info'
     cats_colors = get_cats_colors()
 
     # count up colors
@@ -18,9 +19,14 @@ def main():
     for color in cats_colors:
         counter[color] += 1
 
+    # if is data in the db, reset it
+    colors_info = db_table_size(CATS_COLORS_TABLE)
+    if colors_info:
+        reset_table(CATS_COLORS_TABLE)
+
     # insert colors info to db
-    if dict_items_to_db('cat_colors_info', ('color', 'count'), dict(counter)):
-        print('Cats colors info have been successfully added to the database')
+    dict_items_to_db(CATS_COLORS_TABLE, ('color', 'count'), dict(counter))
+    print('Cats colors info have been successfully added to the database')
 
 
 if __name__ == '__main__':
