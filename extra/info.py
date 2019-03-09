89e67a7814a621c70a254cbb9de370f5cbc040de
Igor Kavzov <igor.kavzov@gmail.com>
2019-03-09 17:26:59 +0300

    Fix wrong argv KeyError

--- extra/info.py
+++ extra/info.py
@@ -5,14 +5,17 @@ from utils import db_query_realdict
 
 items = {
     'cats': {
+        'title': 'cats',
         'table': 'cats',
         'line': '| {name:8}  | {color:20} |  {tail_length:^11} |  {whiskers_length:^16} |'
     },
     'colors': {
+        'title': 'colors',
         'table': 'cat_colors_info',
         'line': '| {color:20}  | {count:^5} |'
     },
     'lengths': {
+        'title': 'lengths',
         'table': 'cats_stat',
         'line': '| {:23} |  {:^7}  |'
     }
@@ -40,43 +43,43 @@ def get_hr(line_len, sign='-'):
 
 def print_header(item):
     print()
-    print(item.capitalize())
+    print(item['title'].capitalize())
 
 
 def print_item_lines(item, data):
     first_line = True
-    if item == 'lengths':
-        line_len = len(items[item]['line'].format('', '')) - 2
+    if item['title'] == 'lengths':
+        line_len = len(item['line'].format('', '')) - 2
         for title, value in data.items():
             if first_line:
                 hr = get_hr(line_len, '=')
                 print(hr)
-                print(items[item]['line'].format('Parameter', 'Value'))
+                print(item['line'].format('Parameter', 'Value'))
                 print(hr)
             hr = get_hr(line_len)
-            print(items[item]['line'].format(conv_title(title), conv_list(value)))
+            print(item['line'].format(conv_title(title), conv_list(value)))
             print(hr)
             first_line = False
     else:
-        line_len = len(items[item]['line'].format(**data[0])) - 2
+        line_len = len(item['line'].format(**data[0])) - 2
         for obj in data:
             if first_line:
                 hr = get_hr(line_len, '=')
                 print(hr)
-                print(items[item]['line'].format(**{k: conv_title(k) for k in obj.keys()}))
+                print(item['line'].format(**{k: k.capitalize() for k in obj.keys()}))
                 print(hr)
             if not data:
                 return
             hr = get_hr(line_len)
-            print(items[item]['line'].format(**obj))
+            print(item['line'].format(**obj))
             print(hr)
             first_line = False
 
 
 def print_item(item):
-    data = db_query_realdict('SELECT * FROM {}'.format(items[item]['table']), many=get_many(item))
+    data = db_query_realdict('SELECT * FROM {}'.format(item['table']), many=get_many(item['title']))
     if not data:
-        print('There is no {} info in the database'.format(item))
+        print('There is no {} info in the database'.format(item['title']))
         return
 
     print_header(item)
@@ -86,14 +89,14 @@ def print_item(item):
 
 def main():
     try:
-        item = sys.argv[1]
+        arg = sys.argv[1]
+        item = items[arg]
     except IndexError:
         print('Error: no any parameter in the command')
         return
     except KeyError:
-        print('Error: unknown parameter "{}"'.format(item))
+        print('Error: unknown parameter "{}"'.format(arg))
         return
-
     print_item(item)
 
 
