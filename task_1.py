39899e65b0574a2bd7e3ed48cf1fe511f076c2bc
Igor Kavzov <igor.kavzov@gmail.com>
2019-03-11 15:27:48 +0300

    Update sql query

--- task_1.py
+++ task_1.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python
 from collections import Counter
-from utils import db_query, dict_items_to_db, db_table_size, reset_table
+from utils import db_query_realdict, dict_items_to_db, db_table_size, reset_table
 from settings import CATS_TABLE
-
-
-def get_cats_colors():
-    """ Gets all colors of cats and return them as list """
-    data = db_query('SELECT color FROM {}'.format(CATS_TABLE))
-    return [color[0] for color in data]
 
 
 def main():
     CATS_COLORS_TABLE = 'cat_colors_info'
-    cats_colors = get_cats_colors()
 
-    # count up colors
-    counter = Counter()
-    for color in cats_colors:
-        counter[color] += 1
+    # get list of dicts {color: count}
+    data = db_query_realdict('SELECT color, COUNT(*) FROM {} GROUP BY color'.format(CATS_TABLE))
+
+    # convert to one dict (for dict_items_to_db())
+    counters = {d['color']: d['count'] for d in data}
 
     msg = 'Cats color info have been '
     # if is data in the db, reset it
@@ -29,7 +23,7 @@ def main():
         msg += 'added to the database'
 
     # add or update colors info in the database
-    dict_items_to_db(CATS_COLORS_TABLE, ('color', 'count'), dict(counter))
+    dict_items_to_db(CATS_COLORS_TABLE, ('color', 'count'), counters)
     print(msg)
 
 
