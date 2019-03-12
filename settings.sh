b554a77df20ad678239e3e7cbc085f9b86eb97ef
Igor Kavzov <igor.kavzov@gmail.com>
2019-03-12 17:51:49 +0300

    Fix ln for task 5

--- settings.sh
+++ settings.sh
@@ -6,7 +6,7 @@ ln -s /wg_test_tasks/task_1.py /usr/local/bin/task1
 ln -s /wg_test_tasks/task_2.py /usr/local/bin/task2
 ln -s /wg_test_tasks/task_3.py /usr/local/bin/task3
 ln -s /wg_test_tasks/task_4.py /usr/local/bin/task4
-ln -s /wg_test_tasks/task_4.py /usr/local/bin/task5
+ln -s /wg_test_tasks/task_5.py /usr/local/bin/task5
 
 ln -s /wg_test_tasks/tests/tests.sh /usr/local/bin/tests
 
