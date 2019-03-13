ee8956fc8a540c3f435cddd2d4d6c442be404e40
Igor Kavzov <igor.kavzov@gmail.com>
2019-03-13 18:40:48 +0300

    Fix path check: startswith -> == ; implemented f-strings

--- task_4.py
+++ task_4.py
@@ -18,23 +18,22 @@ class GETQuery:
     def _check_path(self, path):
         """ Validate query path and add error message if it invalid """
         # query path must starts with '/cats'
-        if not path.startswith(self.VALID_PATH):
-            return "Error: invalid path '{}'. Expected '{}'".format(path, self.VALID_PATH)
+        if not path == self.VALID_PATH:
+            return f"Error: invalid path '{path}'. Expected '{self.VALID_PATH}'"
 
     def _check_params(self, params):
         """ Validate every query parameter and add error message for every invalid value """
         # query parameters must be from the valid query parameters list
         for param in params:
             if not (param in self.VALID_QUERY_PARAMS):
-                return "Error: invalid query parameter '{}'. Allowed parameters: {}".\
-                    format(param, ', '.join(self.VALID_QUERY_PARAMS))
+                return f"Error: invalid query parameter '{param}'. Allowed parameters are: {', '.join(self.VALID_QUERY_PARAMS)}"
 
     def _check_attrs(self, attrs, valid_attrs):
         """ Validate every 'attribute' value and add error message for every invalid value """
         # values of parameter 'attribute' must be from the valid attributes values list
         for attr in attrs:
             if not (attr in valid_attrs):
-                return "Error: invalid attribute '{}'. Allowed attributes: {}".format(attr, ', '.join(valid_attrs))
+                return f"Error: invalid attribute '{attr}'. Allowed attributes are: {', '.join(valid_attrs)}"
 
     def _check_order(self, query_params):
         """ Validate 'order' value and add error message for every invalid case """
@@ -44,40 +43,39 @@ class GETQuery:
         else:
             # 'order' must be only one
             if len(query_params['order']) > 1:
-                return "Error: {} 'order' parameters given. 1 expected".format(len(query_params['order']))
+                return f"Error: {len(query_params['order'])} 'order' parameters given. 1 expected"
             else:
                 # 'order' must have only 'asc' or 'desc' value
                 order = query_params['order'][0]
                 if not (order in self.VALID_ORDER_VALUES):
-                    return "Error: invalid order '{}'. Allowed values: {}.".\
-                        format(order, ', '.join(self.VALID_ORDER_VALUES))
+                    return f"Error: invalid order '{order}'. Allowed values are: {', '.join(self.VALID_ORDER_VALUES)}."
 
     def _check_offset(self, offset_list, cats_number):
         """ Check 'offset' value and add error message for every invalid case """
         # 'offset' must be only one
         if len(offset_list) > 1:
-            return "Error: {} 'offset' parameters given. 1 expected".format(len(offset_list))
+            return f"Error: {len(offset_list)} 'offset' parameters given. 1 expected"
         else:
             offset = offset_list[0]
             # 'offset' must be integer
             if not offset.isdigit():
-                return "Error: invalid offset '{}'. Integer expected.".format(offset)
+                return f"Error: invalid offset '{offset}'. Integer expected."
             else:
                 # 'offset' must be < records in table otherwise nothing to output
                 if int(offset) >= cats_number:
-                    return "Warning: There is no results because offset {} greater than the maximum of {}".\
-                        format(offset, cats_number - 1)
+                    return f"Warning: There is no results because offset {offset} " \
+                           f"greater than the maximum of {cats_number-1}"
 
     def _check_limit(self, limit_list):
         """ Check 'limit' value and add error message for every invalid case """
         # 'limit' must be only one
         if len(limit_list) > 1:
-            return "Error: {} 'limit' parameters given. 1 expected".format(len(limit_list))
+            return f"Error: {len(limit_list)} 'limit' parameters given. 1 expected"
         else:
             limit = limit_list[0]
             # 'limit' must be integer
             if not limit.isdigit():
-                return "Error: invalid limit '{}'. Integer expected.".format(limit)
+                return f"Error: invalid limit '{limit}'. Integer expected."
 
     def check(self, query_string, valid_attrs, cats_number):
         """
@@ -137,29 +135,29 @@ class Task4RequestHandler(BaseHTTPRequestHandler):
 
     def _set_sql_query(self, query_params):
         """ Return sql query string from valid query params """
-        query = 'SELECT * FROM {}'.format(CATS_TABLE)
+        query = f'SELECT * FROM {CATS_TABLE}'
         # attributes in query string
         if query_params.get('attribute'):
             query += ' ORDER BY '
             # may be 1>= attrs
             for attr in query_params['attribute']:
-                query += '{}, '.format(attr)
+                query += f'{attr}, '
             query = query.rstrip(', ')
 
         # order in query string
         if query_params.get('order'):
-            order = query_params['order'][0]
-            query += ' {}'.format(order.upper())
+            order = query_params['order'][0].upper()
+            query += f' {order}'
 
         # offset in query string
         if query_params.get('offset'):
             offset = query_params['offset'][0]
-            query += ' OFFSET {}'.format(offset)
+            query += f' OFFSET {offset}'
 
         # limit in query string
         if query_params.get('limit'):
             limit = query_params['limit'][0]
-            query += ' LIMIT {}'.format(limit)
+            query += f' LIMIT {limit}'
 
         return query
 
